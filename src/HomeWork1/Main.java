88b0753d27758bc61a39165663c6b7650c5563e2
Dor Hanegby <dor@chatleap.com>
2018-12-08 21:35:13 +0200

    first commit

--- src/HomeWork1/Main.java
+++ src/HomeWork1/Main.java
@@ -2,7 +2,32 @@ package HomeWork1;
 
 public class Main {
 
+
+    private class Cell {
+        int state;
+        Cell parent;
+        double maxLikelihood;
+    }
+
+    public static double[][] transitions;
+    public double[][] emissions;
+
+
     public static void main(String[] args) {
-	// write your code here
+	    initModel();
+    }
+
+    public static void initModel() {
+        initTransitions();
+        initEmissions();
+    }
+
+    public static void initTransitions() {
+        transitions = new double[8][8];
+        transitions[1][2] = 0.1;
+    }
+
+    public static void initEmissions() {
+        emissions = new double[8][8];
     }
 }
