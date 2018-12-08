199aa836f7df260be67470fdda147ae91a859525
Dor Hanegby <dor@chatleap.com>
2018-12-08 21:45:54 +0200

    emissions hard coded

--- src/HomeWork1/Main.java
+++ src/HomeWork1/Main.java
@@ -22,6 +22,12 @@ public class Main {
     public static final int S8=7;
 
 
+    public static final int A = 0;
+    public static final int G = 1;
+    public static final int C = 2;
+    public static final int T = 3;
+
+
     public static void main(String[] args) {
 	    initModel();
     }
@@ -47,6 +53,44 @@ public class Main {
 
     public static void initEmissions() {
         emissions = new double[8][4];
-        System.out.println("hey");
+        emissions[S1][A] = 0.3;
+        emissions[S1][G] = 0.2;
+        emissions[S1][C] = 0.2;
+        emissions[S1][T] = 0.3;
+
+        emissions[S2][A] = 1;
+        emissions[S2][G] = 0;
+        emissions[S2][C] = 0;
+        emissions[S2][T] = 0;
+
+        emissions[S3][A] = 0;
+        emissions[S3][G] = 0;
+        emissions[S3][C] = 1;
+        emissions[S3][T] = 0;
+
+        emissions[S4][A] = 0.25;
+        emissions[S4][G] = 0.375;
+        emissions[S4][C] = 0.375;
+        emissions[S4][T] = 0;
+
+        emissions[S5][A] = 0;
+        emissions[S5][G] = 0;
+        emissions[S5][C] = 0;
+        emissions[S5][T] = 1;
+
+        emissions[S6][A] = 0.2;
+        emissions[S6][G] = 0.3;
+        emissions[S6][C] = 0.3;
+        emissions[S6][T] = 0.2;
+
+        emissions[S7][A] = 0.5;
+        emissions[S7][G] = 0;
+        emissions[S7][C] = 0;
+        emissions[S7][T] = 0.5;
+
+        emissions[S8][A] = 0;
+        emissions[S8][G] = 0.5;
+        emissions[S8][C] = 0.5;
+        emissions[S8][T] = 0;
     }
 }
